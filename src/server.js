29c82b0237c071f3b271d67275cb894d22ac240d
lopesboa <lopesboa1@gmail.com>
2019-10-03 23:19:58 -0300

    Creating Trading Rule

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const routes = require("./routes/routes");
+
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(port);
