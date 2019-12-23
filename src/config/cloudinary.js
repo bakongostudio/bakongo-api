e9a2221fa9a2efce68cc450ca1b4082a28f02b35
lopesboa <lopesboa1@gmail.com>
2019-12-23 00:04:41 -0300

    Configuring Cloudinary file

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDNARY_API_NAME,
-  api_key: process.env.CLOUDNARY_API_KEY,
-  api_secret: process.env.CLOUDNARY_API_SECRETE
+  cloud_name: process.env.APP_CLOUDNARY_API_NAME,
+  api_key: process.env.APP_CLOUDNARY_API_KEY,
+  api_secret: process.env.APP_CLOUDNARY_API_SECRETE
 });
 
 module.exports = cloudinary;
