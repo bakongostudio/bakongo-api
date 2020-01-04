34aca30d85ce7eab81c5791c06cb3ae3688332df
lopesboa <lopesboa1@gmail.com>
2020-01-04 15:53:12 -0300

    improvement: changing our husky config

---
+++ build/middlewares/JwtMiddleware.js
@@ -0,0 +1,24 @@
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }const JwtStrategy = require('passport-jwt').Strategy;
+ExtractJwt = require('passport-jwt').ExtractJwt;
+
+var _models = require('../models'); var _models2 = _interopRequireDefault(_models);
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.APP_JWT_SECRET
+};
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      _models2.default.findById(jwt_payload.id)
+        .then(user => {
+          if (user) return done(null, user);
+          return done(null, false);
+        })
+        .catch(err => {
+          return done(err, false, { message: 'Server Error' });
+        });
+    })
+  );
+};
---
+++ build/routes.js
@@ -0,0 +1,11 @@
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }Object.defineProperty(exports, "__esModule", {value: true});var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+
+const routes = _express2.default.Router();
+
+routes.get('/', (req, res, next) => {
+  res.status(200).json({ message: 'We are on!' });
+
+  next();
+});
+
+exports. default = routes;
