61967ae29ba6b606c8d134ad95b7732bccef14d7
lopesboa <lopesboa1@gmail.com>
2020-01-04 15:11:49 -0300

    improvement: finish our User and Token model :heart_eyes:
    In the User model I just create a class with five methods for help us when we are create a user
    model.
    
    addHook method to create a hash of the password before save the data on
    db.
    
    checkPassword method to check if the password insert by the user is the same saved in the
    database.
    
    generateJWT method for creating the auth token using jwt pkg.
    
    genaratePasswordReset
    method for generate a password reset token using crypto pkg and calculates an expiry time (set for
    1h).
    
    getVerificationToken method used for generate a token and creates, returns an instance of
    the Token model.
    
    closes #10

--- src/models/User.js
+++ src/models/User.js
@@ -1,24 +1,79 @@
-import { Model, Sequelize } from 'sequelize';
-import bcrypt from 'bcryptjs';
+import Sequelize, { Model } from 'sequelize'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import crypto from 'crypto'
+
+import Token from './Token'
 
 class User extends Model {
-  static init(sequelize) {
+  static init (sequelize) {
     super.init(
       {
-        name: Sequelize.STRING,
+        first_name: Sequelize.STRING,
+        last_name: Sequelize.STRING,
+        thumb: Sequelize.STRING,
+        document: Sequelize.STRING,
+        genre: Sequelize.INTEGER,
+        birthday: Sequelize.DATE,
+        cell: Sequelize.STRING,
+        phone: Sequelize.STRING,
         email: Sequelize.STRING,
-        password_hash: Sequelize.STRING
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        level: Sequelize.INTEGER,
+        blocking_reason: Sequelize.STRING,
+        is_verified: Sequelize.BOOLEAN,
+        reset_password_token: Sequelize.STRING,
+        reset_password_expires: Sequelize.STRING
       },
       {
         sequelize
       }
-    );
-    return this;
+    )
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8)
+      }
+    })
+
+    return this
   }
 
-  checkPassword(password) {
-    return bcrypt.compare(compare, this.password_hash);
+  checkPassword (password) {
+    return bcrypt.compare(password, this.password_hash)
+  }
+
+  generateJWT () {
+    const today = new Date()
+    const expireDate = new Date(today)
+
+    expireDate.setDate(today.getDate() + 60)
+
+    const payload = {
+      id: this.id,
+      first_name: this.first_name,
+      last_name: this.last_name,
+      email: this.email
+    }
+
+    return jwt.sign(payload, process.env.APP_JWT_SECRET, {
+      expiresIn: parseInt(expireDate.getTime() / 1000, 10)
+    })
+  }
+
+  generatePasswordReset () {
+    this.reset_password_token = crypto.randomBytes(20).toString('hex')
+    this.reset_password_expires = Date.now() + 3600000
+  }
+
+  generateVerificationToken () {
+    const payload = {
+      user_id: this.id,
+      token: crypto.randomBytes(20).toString('hex')
+    }
+    return new Token(payload)
   }
 }
 
-export default User;
+export default User
