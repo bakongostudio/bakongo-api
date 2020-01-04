65d2f819673c21811289e5177de9429eb7351014
lopesboa <lopesboa1@gmail.com>
2020-01-04 15:49:31 -0300

    feat: add pre-push on husky

--- build/database/migrations/20191223015720-create-users.js
+++
@@ -1,9 +0,0 @@
-"use strict";'use strict';
-
-module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {}),
-
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('users');
-  }
-};
